eabd4faa6d75c2b11f4be4bf68b99d4389c2de91
MithYadav <yadav.kumar0118@gmail.com>
2026-04-17 16:26:55 +0530

    initial commit spring api gateway

---
+++ src/main/java/com/example/test/StreamLambdaHandler.java
@@ -0,0 +1,35 @@
+package com.example.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+
+	private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+	static {
+		try {
+			// Load your Spring Boot Application class here
+			handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(SpringBootApiGatewayApplication.class);
+		} catch (ContainerInitializationException e) {
+			throw new RuntimeException("Could not initialize Spring Boot application", e);
+		}
+	}
+
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+		// TODO Auto-generated method stub
+
+		handler.proxyStream(inputStream, outputStream, context);
+
+	}
+
+}

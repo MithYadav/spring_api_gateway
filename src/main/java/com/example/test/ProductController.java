a1ef7cd0b1fb8d9e2f80b90de872133e91015aa5
MithYadav <yadav.kumar0118@gmail.com>
2026-04-17 17:46:06 +0530

    adeed API for product controller

--- src/main/java/com/example/test/ProductController.java
+++ src/main/java/com/example/test/ProductController.java
@@ -1,5 +1,25 @@
 package com.example.test;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
 public class ProductController {
+
+	@GetMapping("/hello")
+	public Map<String, String> sayHello() {
+		return Map.of("message", "Hello from Spring Boot 3 & Java 21!", "status", "success");
+	}
+
+	@PostMapping("/echo")
+	public Map<String, Object> echoData(@RequestBody Map<String, Object> input) {
+		return Map.of("received", input, "processedBy", "AWS Lambda");
+	}
 
 }
